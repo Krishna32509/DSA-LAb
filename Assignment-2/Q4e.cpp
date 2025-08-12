4cb6c46175de39ffe858ae8bdcba462d8917ba42
Krishna32509 <krishnasuneja325@gmail.com>
2025-08-13 00:08:45 +0530

    Update Q4e.cpp

--- Assignment-2/Q4e.cpp
+++ Assignment-2/Q4e.cpp
@@ -11,7 +11,7 @@ void convertToLowercase(char str1[], int n) {
 }
 
 int main() {
-    char str1[10] = "YAsh";
+    char str1[10] = "KRishna";
     int n = strlen(str1);
     convertToLowercase(str1, n);
     cout << str1 << endl;
