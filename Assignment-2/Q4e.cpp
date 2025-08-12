6e5976221c7d7149a81885209a6742e1c9c1d269
Krishna32509 <krishnasuneja325@gmail.com>
2025-08-13 00:03:09 +0530

    Add files via upload

---
+++ Assignment-2/Q4e.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstring>
+#include <cctype> // for tolower
+
+using namespace std;
+
+void convertToLowercase(char str1[], int n) {
+    for (int i = 0; i < n; i++) {
+        str1[i] = tolower(str1[i]); // convert each char to lowercase
+    }
+}
+
+int main() {
+    char str1[10] = "YAsh";
+    int n = strlen(str1);
+    convertToLowercase(str1, n);
+    cout << str1 << endl;
+    return 0;
+}
