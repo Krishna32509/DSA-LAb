4a6257c03f91983ca600a81f8a2546cad83584af
Krishna32509 <krishnasuneja325@gmail.com>
2025-08-13 10:44:29 +0530

    Q5a.cpp

---
+++ Assignment-2/Q5a.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size of square matrix: ";
+    cin >> n;
+
+    int diag[n]; // only store diagonal elements
+
+    cout << "Enter " << n << " diagonal elements:\n";
+    for (int i = 0; i < n; i++)
+        cin >> diag[i];
+
+    cout << "Diagonal Matrix:\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i == j) cout << diag[i] << " ";
+            else cout << "0 ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
